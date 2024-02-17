165286acfe1decf02b3f42dc82f70430e99bf205
Larry Han <larry.han.au@gmail.com>
2024-02-17 19:03:19 +1100

    feature: init commit

---
+++ src/main/java/com/codetest/timbercalculatorbe/TimberCalculatorBeApplication.java
@@ -0,0 +1,13 @@
+package com.codetest.timbercalculatorbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimberCalculatorBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimberCalculatorBeApplication.class, args);
+    }
+
+}
